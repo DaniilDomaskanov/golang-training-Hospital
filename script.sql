a130fcb6e25bc8526db36b589fc41bb471793f67
Damaskanau Daniil <ddamaskanau@gmail.com>
2021-04-18 23:44:18 +0300

    add correct table and columns names

--- script.sql
+++ script.sql
@@ -1,119 +1,119 @@
-CREATE TABLE Speciality 
+CREATE TABLE speciality
 (
-	IdSpeciality SERIAL PRIMARY KEY,
-	SpecialityName CHARACTER VARYING(50) NOT NULL
+	speciality_id SERIAL PRIMARY KEY,
+	speciality_name CHARACTER VARYING(50) NOT NULL
 );
-CREATE TABLE Desease 
+CREATE TABLE disease
 (
-	IdDesease SERIAL PRIMARY KEY,
-	DeseaseName CHARACTER VARYING(50) NOT NULL
+	disease_id SERIAL PRIMARY KEY,
+	disease_name CHARACTER VARYING(50) NOT NULL
 );
-CREATE TABLE Gender 
+CREATE TABLE gender
 (
-	IdGender SERIAL PRIMARY KEY,
-	GenderName CHARACTER VARYING(10) NOT NULL
+	gender_id SERIAL PRIMARY KEY,
+	gender_name CHARACTER VARYING(10) NOT NULL
 );
-CREATE TABLE Doctor
+CREATE TABLE doctor
 (
-	IdDoctor SERIAL PRIMARY KEY,
-	Fname CHARACTER VARYING(50) NOT NULL,
-	Lname CHARACTER VARYING(50) NOT NULL,
-	DateOfBirth DATE NOT NULL,
-	Salary NUMERIC NOT NULL,
-	CurrentBuzyState BOOLEAN NOT NULL,
-	IdGender INTEGER REFERENCES Gender (IdGender) ON DELETE CASCADE,
-	IdSpeciality INTEGER REFERENCES Speciality (IdSpeciality) ON DELETE CASCADE
+	doctor_id SERIAL PRIMARY KEY,
+	first_name CHARACTER VARYING(50) NOT NULL,
+	last_name CHARACTER VARYING(50) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	salary NUMERIC NOT NULL,
+	current_busy_state BOOLEAN NOT NULL,
+	gender_id INTEGER REFERENCES gender (gender_id) ,
+    speciality_id INTEGER REFERENCES speciality (speciality_id)
 );
-CREATE TABLE Nurse
+CREATE TABLE nurse
 (
-	IdNurse SERIAL PRIMARY KEY,
-	Fname CHARACTER VARYING(50) NOT NULL,
-	Lname CHARACTER VARYING(50) NOT NULL,
-	DateOfBirth DATE NOT NULL,
-	CountPatient NUMERIC NOT NULL,
-	IsBuzy BOOLEAN NOT NULL,
-	IdGender INTEGER REFERENCES Gender (IdGender) ON DELETE CASCADE,
-	IdSpeciality INTEGER REFERENCES Speciality (IdSpeciality) ON DELETE CASCADE
+	nurse_id SERIAL PRIMARY KEY,
+    first_name CHARACTER VARYING(50) NOT NULL,
+    last_name CHARACTER VARYING(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+	count_patient NUMERIC NOT NULL,
+	is_busy BOOLEAN NOT NULL,
+	gender_id INTEGER REFERENCES gender (gender_id),
+	speciality_id INTEGER REFERENCES speciality (speciality_id)
 );
-CREATE TABLE Ward
+CREATE TABLE ward
 (
-	IdWard SERIAL PRIMARY KEY,
-	Name CHARACTER VARYING(50) NOT NULL,
-	Location CHARACTER VARYING(50) NOT NULL,
-	QuartializationDate DATE NOT NULL,
-	Capacity NUMERIC NOT NULL,
-	IsEmpty BOOLEAN NOT NULL,
-	IdDoctor INTEGER REFERENCES Doctor (IdDoctor) ON DELETE CASCADE,
-	IdNurse INTEGER REFERENCES Nurse (IdNurse) ON DELETE CASCADE
+	ward_id SERIAL PRIMARY KEY,
+	name CHARACTER VARYING(50) NOT NULL,
+	location CHARACTER VARYING(50) NOT NULL,
+	quartialization_date DATE NOT NULL,
+	capacity NUMERIC NOT NULL,
+	is_empty BOOLEAN NOT NULL,
+	doctor_id INTEGER REFERENCES doctor (doctor_id),
+	nurse_id INTEGER REFERENCES nurse (nurse_id)
 );
-CREATE TABLE Patient
+CREATE TABLE patient
 (
-	IdPatient SERIAL PRIMARY KEY,
-	Fname CHARACTER VARYING(50) NOT NULL,
-	Lname CHARACTER VARYING(50) NOT NULL,
-	DateOfBirth DATE NOT NULL,
-	NumberDays NUMERIC NOT NULL,
-	IsSick BOOLEAN NOT NULL,
-	IdDoctor INTEGER REFERENCES Doctor (IdDoctor) ON DELETE CASCADE,
-	IdNurse INTEGER REFERENCES Nurse (IdNurse) ON DELETE CASCADE,
-	IdWard INTEGER REFERENCES Ward (IdWard) ON DELETE CASCADE,
-	IdGender INTEGER REFERENCES Gender (IdGender) ON DELETE CASCADE,
-	IdDesease INTEGER REFERENCES Desease (IdDesease) ON DELETE CASCADE
+	patient_id SERIAL PRIMARY KEY,
+	first_name CHARACTER VARYING(50) NOT NULL,
+	last_name CHARACTER VARYING(50) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	number_days NUMERIC NOT NULL,
+	is_sick BOOLEAN NOT NULL,
+	doctor_id INTEGER REFERENCES doctor (doctor_id),
+    nurse_id INTEGER REFERENCES nurse (nurse_id),
+    ward_id INTEGER REFERENCES ward (ward_id),
+    gender_id INTEGER REFERENCES gender (gender_id),
+    disease_id INTEGER REFERENCES disease (disease_id)
 );
-CREATE TABLE Service
+CREATE TABLE service
 (
-	IdService SERIAL PRIMARY KEY,
-	Name CHARACTER VARYING(50) NOT NULL,
-	DateOfBirth DATE NOT NULL,
-	Price NUMERIC NOT NULL,
-	IsDone BOOLEAN NOT NULL,
-	IdDoctor INTEGER REFERENCES Doctor (IdDoctor) ON DELETE CASCADE,
-	IdPatient INTEGER REFERENCES Patient (IdPatient) ON DELETE CASCADE
+	service_id SERIAL PRIMARY KEY,
+	name CHARACTER VARYING(50) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	price NUMERIC NOT NULL,
+	is_done BOOLEAN NOT NULL,
+    doctor_id INTEGER REFERENCES doctor (doctor_id),
+    patient_id INTEGER REFERENCES patient (patient_id)
 );
-INSERT INTO Speciality (SpecialityName) VALUES 
+INSERT INTO speciality (speciality_name) VALUES
 ('General Surgery'),
 ('Main Nurse'),
 ('Assistant nurse'),
 ('Neurology'),
 ('Infectious Diseases');
-INSERT INTO Desease (DeseaseName) VALUES 
+INSERT INTO disease (disease_name) VALUES
 ('Anthrax'),
 ('Botulism'),
 ('Brucellosis'),
 ('COVID-19'),
 ('Ebola virus');
-INSERT INTO Gender (GenderName) VALUES 
+INSERT INTO gender (gender_name) VALUES
 ('Female'),
 ('Male');
-INSERT INTO Doctor (Fname,Lname,DateOfBirth,Salary,CurrentBuzyState
-					,IdGender,IdSpeciality) VALUES 
+INSERT INTO doctor (first_name,last_name,date_of_birth,salary,current_busy_state
+					,gender_id,speciality_id) VALUES
 ('Daniil','Domaskanov','2000-09-12',1250.12,FALSE,2,1),
 ('Dmitry','Putkov','1999-02-13',5000.54,TRUE,2,4),
 ('Victor','Pashkevich','2001-07-25',570.14,FALSE,2,5),
 ('Eva','Dushkevich','2002-10-22',790.77,TRUE,1,1),
 ('Nikita','Miladovski','1995-04-18',680.56,FALSE,2,5);
-INSERT INTO Nurse (Fname,Lname,DateOfBirth,CountPatient,isBuzy
-					,IdGender,IdSpeciality) VALUES 
+INSERT INTO nurse (first_name,last_name,date_of_birth,count_patient,is_busy
+					,gender_id,speciality_id) VALUES
 ('Anya','Udodva','1988-06-9',5,TRUE,1,2),
 ('Natalya','Seliverstova','1976-01-29',10,TRUE,1,3),
 ('Evgenia','Gapanovich','1990-03-15',7,TRUE,1,3),
 ('Alina','Kosenkova','2000-09-12',11,TRUE,1,3),
 ('Veronika','Dolgolapteva','1991-02-10',12,FALSE,2,2);
-INSERT INTO Ward (Name,Location,QuartializationDate,Capacity,IsEmpty
-					,IdDoctor,IdNurse) VALUES 
+INSERT INTO ward (name,location,quartialization_date,capacity,is_empty
+					,doctor_id,nurse_id) VALUES
 ('12 ward','1-st floor','2020-01-10',6,FALSE,1,1),
 ('24 ','2-st floor','2020-01-12',4,FALSE,2,2),
 ('36','3-st floor','2020-01-13',3,FALSE,3,3),
 ('49','4-st floor','2020-01-14',8,TRUE,4,4),
 ('55','5-st floor','2020-01-16',2,FALSE,5,5);
-INSERT INTO Patient (Fname,Lname,DateOfBirth,NumberDays,IsSick
-					,IdDoctor,IdNurse,IdWard,IdGender,IdDesease) VALUES 
+INSERT INTO patient (first_name,last_name,date_of_birth,number_days,is_sick
+					,doctor_id,nurse_id,ward_id,gender_id,disease_id) VALUES
 ('Amari','Fitzgerald','1999-02-12',14,TRUE,1,1,1,2,1),
 ('Buddy','Chandler','2000-09-12',12,TRUE,2,2,2,2,2),
 ('Yousaf','Schultz','2001-05-17',10,TRUE,3,3,3,2,3),
 ('Katherine','Mejia','2002-12-14',8,TRUE,4,4,4,1,4),
 ('Esme-Rose','Warner','2003-11-18',2,TRUE,5,5,5,2,5);
-INSERT INTO Service (Name,Price,IsDone,DateOfService,IdDoctor,IdPatient) VALUES 
+INSERT INTO service (name,price,is_done,date_of_birth,doctor_id,patient_id) VALUES
 ('Emergency room',1250.12,TRUE,'1999-02-12',1,1),
 ('Surgical',1400.124,FALSE,'2000-09-12',2,2),
 ('X ray/radiology',1567.19,TRUE,'2001-05-17',3,3),
